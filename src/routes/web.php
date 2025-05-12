acf37ed6e7e5fb17745c2192506f141ff5bb48ad
isii ayaka <ayaka950210@gmail.com>
2025-05-12 14:53:00 +0900

    ItemController修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ContactController::class, 'index']);
+Route::get('/', [ItemController::class, 'index']);
