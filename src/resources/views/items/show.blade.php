62401987896793fbf3527e869571af417986fd2c
isii ayaka <ayaka950210@gmail.com>
2025-05-19 22:43:08 +0900

    item-detail.css 作成

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -1,32 +1,43 @@
 @extends('layout.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/item-detail.css') }}">
+@endsection
+
 @section('content')
 <div class="item-detail-container">
-    <div class="item-image">
-        <img src="{{ asset('storage/' . $item->image) }}" alt="商品画像">
+    <div class="item-detail-header">
+        <div class="item-image">
+            <img src="{{ asset('storage/' . $item->image) }}" alt="商品画像">
+        </div>
+        <div class="item-info">
+            <h2>{{ $item->name }}</h2>
+                <p>¥{{ number_format($item->price) }} (税込)</p>
+                <button class="purchase-btn">購入手続きへ</button>
+        </div>
     </div>
-    <div class="item-info">
-        <h2>{{ $item->name }}</h2>
-        <p>¥{{ number_format($item->price) }} (税込)</p>
-        <button class="purchase-btn">購入手続きへ</button>
 
-        <h3>商品説明</h3>
-        <p>{{ $item->description }}</p>
-
-        <h3>商品情報</h3>
-        <ul>
-            <li>カテゴリー: {{ $item->category }} </li>
-            <li>商品の状態: {{ $item->condition }}</li>
-        </ul>
-
-        <h3>コメント</h3>
-        <div class="comments">
-            // コメント表示エリア
+        <div class="item-description">
+            <h3>商品説明</h3>
+            <p>{{ $item->description }}</p>
         </div>
 
-        <h3>商品のコメント</h3>
-        <textarea placeholder="ここにコメントを入力"></textarea>
-        <button class="comment-submit-btn">コメントを投稿する</button>
-    </div>
+        <div class="item-details">
+            <h3>商品情報</h3>
+            <ul>
+                <li>カテゴリー: {{ $item->category }} </li>
+                <li>商品の状態: {{ $item->condition }}</li>
+            </ul>
+        </div>
+
+        <div class="item-comments">
+            <h3>コメント</h3>
+            <div class="comments">
+                //コメント一覧表示
+            </div>
+            <h3>商品のコメント</h3>
+            <textarea placeholder="ここにコメントを入力"></textarea>
+            <button class="comment-submit-btn">コメントを投稿する</button>
+        </div>
 </div>
 @endsection
