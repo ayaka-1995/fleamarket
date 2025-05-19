09ab9eea8ef9e25a9700d4ccfc69ee2bea502527
isii ayaka <ayaka950210@gmail.com>
2025-05-19 21:52:23 +0900

    show.blade.php 編集

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -13,6 +13,20 @@
         <h3>商品説明</h3>
         <p>{{ $item->description }}</p>
 
-        
+        <h3>商品情報</h3>
+        <ul>
+            <li>カテゴリー: {{ $item->category }} </li>
+            <li>商品の状態: {{ $item->condition }}</li>
+        </ul>
+
+        <h3>コメント</h3>
+        <div class="comments">
+            // コメント表示エリア
+        </div>
+
+        <h3>商品のコメント</h3>
+        <textarea placeholder="ここにコメントを入力"></textarea>
+        <button class="comment-submit-btn">コメントを投稿する</button>
     </div>
 </div>
+@endsection
